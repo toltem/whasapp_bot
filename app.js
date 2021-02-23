3761f32740f9ce5b45ca42a9f6ada55d4e2d60cb
valentine-mario <valentine13400@gmail.com>
2021-02-23 09:42:49 +0100

    added cors

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(function (req, res, next) {
   next();
 });
 
+
 const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 const Redis = require("ioredis");
 const redis = new Redis(REDIS_URL);
